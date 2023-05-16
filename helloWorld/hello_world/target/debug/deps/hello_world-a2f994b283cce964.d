8a06d4a01bc0e18b54c43962b945b86ecf09f998
gmy <>
2023-05-16 22:19:38 +0800

    hello world

---
+++ helloWorld/hello_world/target/debug/deps/hello_world-a2f994b283cce964.d
@@ -0,0 +1,5 @@
+/Users/gmy/Desktop/rust/helloWorld/hello_world/target/debug/deps/hello_world-a2f994b283cce964: src/main.rs
+
+/Users/gmy/Desktop/rust/helloWorld/hello_world/target/debug/deps/hello_world-a2f994b283cce964.d: src/main.rs
+
+src/main.rs:
---
+++ helloWorld/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/gmy/Desktop/rust/helloWorld/hello_world/target/debug/hello_world: /Users/gmy/Desktop/rust/helloWorld/hello_world/src/main.rs
